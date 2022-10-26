c3c129f443f6a73e0c2aa814f29611913b89eae6
daikimatsu06 <52761513+daikimatsu06@users.noreply.github.com>
2022-10-26 17:36:13 +0900

    Create 001.cpp

---
+++ 001.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int sum = 0;
+    for (int i = 3; i < 1000; ++i) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    std::cout << sum << std::endl;
+    return 0;
+}
+
+// answer: 233168
+// time: 0.012618 s.
